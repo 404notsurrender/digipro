b0526bcc06eb2a25118df102b785bb3217bc7cd6
404notsurrender <muhammad.zulfikar011@binus.ac.id>
2023-11-22 15:46:34 +0700

    Initial commit

---
+++ auth/logout.php
@@ -0,0 +1,22 @@
+<?php
+session_start(); // Mulai sesi jika belum dimulai
+
+// Hapus semua variabel sesi
+$_SESSION = array();
+
+// Hapus cookie sesi jika ada
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Hancurkan sesi
+session_destroy();
+
+// Redirect ke halaman login atau halaman lain yang sesuai
+header("Location: login.php");
+exit();
+?>
