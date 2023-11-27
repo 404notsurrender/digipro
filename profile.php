a8c43bae56f8f2ab0fcc0e2b0aee4f3f49ad1450
404notsurrender <muhammad.zulfikar011@binus.ac.id>
2023-11-27 21:09:47 +0700

    Ravi Rajabi fixing password bugs.

--- profile.php
+++ profile.php
@@ -119,4 +119,4 @@
 </div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min"></script>
